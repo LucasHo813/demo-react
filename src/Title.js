891a9bc1157173dff30e50a51791050c89726fea
LucasHo813 <108063279+LucasHo813@users.noreply.github.com>
2022-08-18 16:20:37 +0800

    final version

---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Title({mainTitle}) {
+    return (
+        <div className='text-center mt-4 fw-bold'>
+            <h1>
+                {mainTitle}
+            </h1>
+        </div>
+    )
+}
